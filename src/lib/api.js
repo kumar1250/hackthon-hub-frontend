bbc3285699bc5865c1d64b3e21e095a414b4077f
kumar yedida <kumaryedida339@gmail.com>
2026-08-05 15:44:02 +0530

    frontend

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://hackthon-hub-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
